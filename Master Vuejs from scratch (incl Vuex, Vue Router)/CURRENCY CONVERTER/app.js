029b7d3bd406798854e80f2c01a6bdc4a7882cdd
Ricky Anwar <rickyanwar777@gmail.com>
2020-05-21 00:57:39 +0800

    Currency Converter Vue JS

--- Master Vuejs from scratch (incl Vuex, Vue Router)/CURRENCY CONVERTER/app.js
+++ Master Vuejs from scratch (incl Vuex, Vue Router)/CURRENCY CONVERTER/app.js
@@ -2,9 +2,10 @@ new Vue({
     el : '#app',
     data:{
       currencies : {},
-      amount : '',
+      amount : 0,
       from : '',
-      to : ''
+      to : '',
+      results : ''
 
     },
     mounted() {
@@ -21,6 +22,20 @@ new Vue({
                 console.log(Response.data.results)
                 this.currencies = Response.data.results
             })
+        },
+        convertCurrencies(){
+          let key = `${this.from}_${this.to}`
+          axios.get(`https://free.currconv.com/api/v7/convert?q=${key}&compact=ultra&apiKey=9c6cc03295bc52d9741e`).then((response)=>{
+            console.log(response.data)
+            console.log(key)
+            this.results = response.data[key]
+          })
+        },
+        disabled(){
+          return this.amount === 0 || !this.amount
+        },
+        calculateResult(){
+          return (Number(this.amount) * this.results).toFixed(3)
         }
     },
 })
