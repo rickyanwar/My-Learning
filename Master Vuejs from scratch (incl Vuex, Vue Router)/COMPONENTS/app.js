ae6d53edfba69449c1c4b7e4788b7acd0b0032cc
Ricky Anwar <rickyanwar777@gmail.com>
2020-05-21 01:37:54 +0800

    Vue Component (Mastering Vue JS)

---
+++ Master Vuejs from scratch (incl Vuex, Vue Router)/COMPONENTS/app.js
@@ -0,0 +1,15 @@
+Vue.component('card',{
+    template :`  <div class="card">
+    <div class="card-body">
+        <h3 class="card-title">Lorem, ipsum dolor. </h3>
+        <div class="card-text">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum dolor, voluptas laborum assumenda doloribus quis? Dignissimos odio similique atque ratione libero enim rem delectus quaerat! Et asperiores nemo quibusdam. Amet possimus debitis corrupti, sit soluta aperiam! Quae nostrum voluptate quisquam nesciunt molestias velit cupiditate unde quidem! Itaque, dolores! Laborum, officiis?
+        </div>
+    </div>
+</div>`
+})
+
+new Vue({
+    el:'#app',
+
+})
