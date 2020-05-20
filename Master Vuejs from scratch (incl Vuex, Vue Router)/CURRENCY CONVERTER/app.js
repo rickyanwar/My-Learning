9a46dc4e8925ddc24e792a277d898f39007b4e8b
Ricky Anwar <rickyanwar777@gmail.com>
2020-05-21 01:18:35 +0800

    Fix On Change with Watcher (Mastering Vue JS)

--- Master Vuejs from scratch (incl Vuex, Vue Router)/CURRENCY CONVERTER/app.js
+++ Master Vuejs from scratch (incl Vuex, Vue Router)/CURRENCY CONVERTER/app.js
@@ -5,7 +5,8 @@ new Vue({
       amount : 0,
       from : '',
       to : '',
-      results : ''
+      results : '',
+      isLoading : false
 
     },
     mounted() {
@@ -24,18 +25,28 @@ new Vue({
             })
         },
         convertCurrencies(){
+          this.isLoading = true
           let key = `${this.from}_${this.to}`
           axios.get(`https://free.currconv.com/api/v7/convert?q=${key}&compact=ultra&apiKey=9c6cc03295bc52d9741e`).then((response)=>{
+            this.isLoading = false
             console.log(response.data)
             console.log(key)
             this.results = response.data[key]
           })
         },
         disabled(){
-          return this.amount === 0 || !this.amount
+          return this.amount === 0 || !this.amount || this.isLoading
         },
         calculateResult(){
           return (Number(this.amount) * this.results).toFixed(3)
         }
+    },
+    watch: {
+      from(){
+        this.results =0
+      },
+      to(){
+        this.results =0
+      }
     },
 })
