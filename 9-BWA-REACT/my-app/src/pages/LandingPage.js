1c152ee0600e669b1b4c8db7de0caf35ac52c5c2
Ricky Anwar <rickyanwar777@gmail.com>
2022-04-01 22:37:30 +0700

    add hero on bwa

--- 9-BWA-REACT/my-app/src/pages/LandingPage.js
+++ 9-BWA-REACT/my-app/src/pages/LandingPage.js
@@ -2,11 +2,14 @@
 import React, { Component } from 'react';
 
 import Header from 'parts/Header'
+import landingPage from 'json/landingPage.json'
+
 
 export default class LandingPage extends Component {
     render() {
         return <>
             <Header {...this.props}></Header>
+            <hero></hero>
         </>
     }
 }
---
+++ 9-BWA-REACT/my-app/src/parts/Hero.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ImageHero from 'assets/images/img-hero.png'
+import ImageHero_ from 'assets/images/img-hero-frame.png'
+import IconCities from 'assets/images/icons/ic_cities.png'
+import IconTraveler from 'assets/images/icons/ic_traveler.png'
+import IconTreasure from 'assets/images/icons/ic_treasure.png'
+
+
+export default function Hero(props) {
+  return (
+    <div>Hero</div>
+  )
+}
