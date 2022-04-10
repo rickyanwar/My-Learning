0485b85be06519826e647beff487ade8e7efea19
rickanwary <rickyanwar777@gmail.com>
2022-04-10 11:08:53 +0800

    add category section BWA react

--- 9-BWA-REACT/my-app/src/pages/LandingPage.js
+++ 9-BWA-REACT/my-app/src/pages/LandingPage.js
@@ -5,7 +5,7 @@ import Header from 'parts/Header'
 import landingPage from 'json/landingPage.json'
 import Hero from 'parts/Hero';
 import MostPicked from 'parts/MostPicked';
-
+import Categories from 'parts/Categories';
 export default class LandingPage extends Component {
 
     constructor(props){
@@ -24,6 +24,7 @@ export default class LandingPage extends Component {
                 data={landingPage.mostPicked}
                 refMostPicked={this.refMostPicked}
             ></MostPicked>
+            <Categories data={landingPage.categories}></Categories>
         </>
     }
 } 
